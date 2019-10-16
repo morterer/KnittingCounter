1959a06739b08000490ae6774d98976e36161960
Eric Morter <mrk9270@R5119708.mfad.mfroot.org>
2019-10-16 12:40:03 -0500

    added block model and Angular Material

---
+++ src/app/models/block.ts
@@ -0,0 +1,18 @@
+export class Block {
+  public color: string = 'New Color';
+  public totalRows: number;
+  public currentRows: number = 0;
+  public active: boolean = true;
+
+  public increment(): void {
+    this.currentRows++;
+    // if currentRows is equal to or greater than totalRows
+    // mark the block as inactive (might be better to call it complete?)
+    if (this.currentRows >= this.totalRows) {
+      this.active = false;
+    }
+  }
+  public decrement(): void {
+    this.currentRows--;
+  }
+}
